5aaa0d4c92250566661f15e2c554fd68d4a2102f
julianrolon <julianrolonramirez@gmail.com>
2026-02-16 16:43:42 -0500

    Spring initializr

---
+++ src/main/java/io/github/julianrolon/beer/BeerSpringHexagonalApplication.java
@@ -0,0 +1,13 @@
+package io.github.julianrolon.beer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerSpringHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerSpringHexagonalApplication.class, args);
+	}
+
+}
